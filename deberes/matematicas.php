986c9f41191f7f6511370268d3a22320ec99a6ab
María <mchaparrocaballero@gmail.com>
2025-09-27 15:13:30 +0200

    organizamos matematicas

--- deberes/matematicas.php
+++ deberes/matematicas.php
@@ -15,7 +15,9 @@
         $b = $_GET['b'];
         $c = $_GET['c'];
 
-      function ecuaciones($a,$b,$c){
+        ////ejercicio 1//
+
+      function ecuaciones1($a,$b,$c){
         $discriminante = ($b**2) - (4*$a*$c);
         if($discriminante > 0){
             $x1 = (-$b + sqrt($discriminante)) / (2*$a);
@@ -28,8 +30,36 @@
             return "No hay soluciones reales.";
         }
       }
+
+      //echo ecuaciones1($a,$b,$c);
+
+
+
+      //ejercicio 2//
+      function ecuaciones($a,$b,$c){
+
+        $soluciones = [];
+
+        $discriminante = ($b**2) - (4*$a*$c);
+        if($discriminante > 0){
+            $x1 = (-$b + sqrt($discriminante)) / (2*$a);
+            $x2 = (-$b - sqrt($discriminante)) / (2*$a);
+            $soluciones[] = $x1;
+            $soluciones[] = $x2;
+
+            return $soluciones;
+        }elseif($discriminante == 0){
+            $x = -$b / (2*$a);
+            $soluciones[] = $x;
+            return $soluciones;
+        }else{
+            return "false";
+        }
+      }
       
       echo ecuaciones($a,$b,$c);
+      
+      
       ?>
 </body>
 </html>
