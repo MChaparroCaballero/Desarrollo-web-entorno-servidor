87c1a5f7910339c1f6fb82db04fa4595a5c0bb10
María <mchaparrocaballero@gmail.com>
2025-10-13 22:33:29 +0200

    Delete primera_entrega directory

--- primera_entrega/entregaWeb/datos.php
+++
@@ -1,5 +0,0 @@
-<?php
-$titulo = "Mi página con imagen online";
-$alt = "Imagen cargada desde internet";
-$imagen = "https://img.freepik.com/foto-gratis/diseno-collage-personas_23-2148888275.jpg";
-?>
